edf9c3002a6947b335dc354cb447cb7261d6270a
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-10 09:30:31 -0700

    script for 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,16 +9,16 @@
 int main(void)
 
 {
-	int i;
-	double d;
-	char c;
-	float k;
-	long int j;
+	char i;
+	int d;
+	long int c;
+	double k;
+	float j;
 
-	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(k));
-	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(j));
 	return (0);
 }
