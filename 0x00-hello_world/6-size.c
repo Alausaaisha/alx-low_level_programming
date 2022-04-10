f1136cbfd6674378aa0e4376812b64053afb0f3b
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-10 08:49:27 -0700

    script for 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+
+{
+	int i;
+	double d;
+	char c;
+	float k;
+	long j;
+
+	printf("size of a int: %lu byte(s)\n", (unsigned long)size of (i));
+	printf("size of a double: %lu byte(s)\n", (unsigned long)size of (d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)size of (k));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)size of (j));
+	return (0);
+}
