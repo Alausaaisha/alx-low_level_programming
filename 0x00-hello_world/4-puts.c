648971755497ec5f2acfbbbced9278b6229251e8
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-09 18:14:52 -0700

    script for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
