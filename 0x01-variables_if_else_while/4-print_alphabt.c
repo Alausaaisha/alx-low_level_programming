21b47a1036888d90be94f76d72f24cec44de3da5
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-18 15:31:40 -0700

    script for 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet from a to z
+ * except q and e
+ * Return: zero
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+}
