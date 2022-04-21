bf1b2953593381e3906f910f830e9ebbf591d95d
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-21 16:11:18 -0700

    script for 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabets in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d++)
+		_putchar ('d');
+	_putchar ('\n');
+	return (0);
