25009239565c734d1de3ea87034335c2b6b5c67b
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-22 15:36:33 -0700

    script for 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: integer a
+ * @b: integer b
+ * Description: function that adds two integers and returns the result
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
