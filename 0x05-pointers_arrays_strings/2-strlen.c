46289e96021d787313aa78e1d63082f70680dccb
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-25 22:06:39 -0700

    script for 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _strlen -  returns the length of a string
+ * @*s: string who length is to be printed
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	len = strlen(s);
+	
+	{
+	_putchar("strlen(s) :  %d\n", len);
+	}
+}
