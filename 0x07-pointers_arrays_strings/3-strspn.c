f76be5d42ad0ec6dd2810663c712e9cf209d3078
Alausaaisha <qtaisha.ao@gmail.com>
2022-04-27 22:21:18 -0700

    script for 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - Returns the number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ * @s: full string
+ * @accept: substring to return
+ * Return: length of substring
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j<= i; j++)
+		{
+			if (accept[j] == '\0')
+				return (j);
+		}
+		i++;
+	}
+	return (j);
+
+}
