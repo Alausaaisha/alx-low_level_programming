ad42e882c5e3da2283ef75ce6284fb64240721fa
Alausaaisha <qtaisha.ao@gmail.com>
2022-05-02 08:15:45 -0700

    script for 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string
+ * s: string to be printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if(*s == '\0')
+		putchar('\n');
+	_puts_recursion(*s);
+}
