bdcc351bacf4b2ab7759a314ddc122be0cca4f5e
Alausaaisha <qtaisha.ao@gmail.com>
2022-05-14 23:11:55 -0700

    function to check for digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: digit to be checked
+ * Return: 1 if true and 0 if false
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
