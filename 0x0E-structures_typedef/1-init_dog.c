7f11467cfcd20beaca23b9682207524dcf64d7c6
Alausaaisha <qtaisha.ao@gmail.com>
2022-05-15 08:15:52 -0700

    function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * init_dog - function that initializes the dog info
+ * @d: pointer to a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
