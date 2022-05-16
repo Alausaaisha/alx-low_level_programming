de9117607846135cd7c3ffa6f3a8225335892ed6
Alausaaisha <qtaisha.ao@gmail.com>
2022-05-15 22:18:20 -0700

    function that searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
  * @array: array name
  * @size: size of the array
  * @cmp: funtion pointer
- * Return: -1 if size <= 0, 1if no element is found
+ * Return: -1 if size <= 0, -1 if no element is found
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (-1);
 		for (; i < size; i++)
 		{
-			if (cmp(array[[i] != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
