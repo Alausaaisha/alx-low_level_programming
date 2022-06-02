2a981602cc40011ee1eaaeb202f54765ac37a259
Alausaaisha <qtaisha.ao@gmail.com>
2022-06-02 04:14:19 -0700

    prints a string followed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts -  prints a string, followed by a new line
+ * @str: string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i; strlen = *(str + i)
+
+		for (i = 0; strlen != 0; i++)
+		{
+			_puts(str[i]);
+		}
+}
